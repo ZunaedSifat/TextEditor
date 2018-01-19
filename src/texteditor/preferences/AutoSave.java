abaeab4583fab37d35d437af39ed51cd61919a35
Zunaed Karim Sifat <zunaed.sifat@gmail.com>
2018-01-19 12:55:54 +0600

    autosave background class done

--- src/texteditor/preferences/AutoSave.java
+++ src/texteditor/preferences/AutoSave.java
@@ -1,4 +1,52 @@
 package texteditor.preferences;
 
-public class AutoSave {
+import javafx.scene.control.Tab;
+import texteditor.editor.EditorTab;
+import texteditor.file.SaveFile;
+import texteditor.main.CodeEditor;
+
+public class AutoSave implements Runnable {
+
+    private boolean autosaveOn;
+    private static AutoSave object;
+    private int delay;
+
+    private AutoSave() {
+
+        autosaveOn = false;
+        //todo: read from file
+    }
+
+    @Override
+    public void run() {
+
+        if (autosaveOn) {
+
+            for (Tab tab : CodeEditor.getTabPane().getTabs()) {
+
+                EditorTab editor = (EditorTab) tab.getContent();
+                if (editor.getPath() != null)
+                    SaveFile.saveFile(tab);
+            }
+        }
+
+        try {
+            Thread.sleep(delay);
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void autoSaveEnable(int delay) {
+
+        if (object == null) object = new AutoSave();
+        object.delay = delay;
+        object.autosaveOn = true;
+    }
+
+    public void setAutosaveDisable() {
+
+        if (object == null) object = new AutoSave();
+        object.autosaveOn = false;
+    }
 }
