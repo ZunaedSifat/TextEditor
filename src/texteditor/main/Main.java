5aeae14c34b58c09b1f710f0bda9eb3d4d3bdd82
627 <rebornplusplus@gmail.com>
2018-01-19 10:55:42 +0600

    workspace fucking changed

--- src/texteditor/main/Main.java
+++ src/texteditor/main/Main.java
@@ -5,7 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
