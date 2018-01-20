7e067c53ff9d726a8b959c5b086ef48bbe350129
Zunaed Karim Sifat <zunaed.sifat@gmail.com>
2018-01-20 19:04:03 +0600

    main done

--- src/texteditor/main/Main.java
+++ src/texteditor/main/Main.java
@@ -2,33 +2,40 @@ package texteditor.main;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import texteditor.file.OpenRecent;
+import texteditor.file.SaveAllFiles;
+import texteditor.file.SaveFile;
 import texteditor.preferences.AutoSave;
 import texteditor.preferences.PreferenceData;
 
 public class Main extends Application{
 
     public static void main(String[] args) {
+        Initializer.initialize();
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Thread autoSave = new Thread(AutoSave.getObject());
-        autoSave.start();
-
-        VBox layout = new VBox(10);
-        layout.getChildren().addAll(MenuBarCreator.getMenuBar(), CodeEditor.getTabPane());
+        BorderPane layout = new BorderPane();
+        layout.setTop(MenuBarCreator.getMenuBar());
+        layout.setCenter(CodeEditor.getTabPane());
 
         primaryStage.setTitle("Text Editor");
         primaryStage.setScene(new Scene(layout, 640, 480));
-        primaryStage.setOnCloseRequest(e -> {
-            PreferenceData.writeData();
-            PreferenceData.setProgramClosed(true);
-            System.exit(0);
-        });
+
+        primaryStage.setOnCloseRequest(e -> close());
         primaryStage.show();
+    }
+
+    public static void close() {
+        PreferenceData.writeData();
+        PreferenceData.setProgramClosed(true);
+        OpenRecent.writeData();
+        System.exit(0);
     }
 }
